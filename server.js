5f251d418eed89f09f214f15be3a05e9a451edb1
lalfag <luis.alfageme.m@gmail.com>
2026-06-12 22:17:44 +0200

    fix: puerto dinámico y script start para Railway

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const {
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
